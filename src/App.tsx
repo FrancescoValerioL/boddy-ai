88df6f78ae987ae86bab854960af8165eb90a3cb
Francesco Valerio <francescovalerio.l@outlook.com>
2024-05-02 12:52:32 +0200

    Home page base and tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
+import { Row, Col, Nav } from "react-bootstrap";
 import "./App.scss";
-import { Button } from "react-bootstrap";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Home from "./Tabs/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="success">Test</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="logo">
+        <Row>
+          <Col sm={12}>
+            <Nav fill variant="underline" className="flex-row">
+              <Nav.Item >
+                <Nav.Link className="colored-tab" eventKey="logo">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={12}>
+            <Tab.Content>
+              <Tab.Pane eventKey="logo"><Home /></Tab.Pane>
+              <Tab.Pane eventKey="first">First tab content</Tab.Pane>
+              <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </div>
   );
 }
