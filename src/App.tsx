d5f2128b5dbe3d951479efa6a9d37fa96df554ef
Francesco Valerio <francescovalerio.l@outlook.com>
2024-05-06 18:49:24 +0200

    Update scss and create buddy bot

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { Row, Col, Nav, Dropdown, NavItem, NavLink, Image } from "react-bootstrap";
 import "./App.scss";
-import Tab from 'react-bootstrap/Tab';
-import logo from "./buddyAiLogo2.svg"
+import Tab from "react-bootstrap/Tab";
+import logo from "./buddyAiLogo2.svg";
 import Home from "./Tabs/Home/Home";
 import { useState } from "react";
 import CreateBuddyBot from "./Tabs/CreateBuddyBot/CreateBuddyBot";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("Account")
-  const defaultPageName: string = "Account"
+  const [selectedPage, setSelectedPage] = useState<string>("Account");
+  const defaultPageName: string = "Account";
   return (
     <div className="App">
-      <Tab.Container id="left-tabs-example" defaultActiveKey="logo">
+      <Tab.Container id="left-tabs-example" defaultActiveKey="logo" transition={false}>
         <Row>
           <Col sm={12}>
             <Nav fill variant="pills" className="flex-row">
@@ -21,25 +21,35 @@ function App() {
                 </Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link onClick={() => setSelectedPage(defaultPageName)} eventKey="first">Tab 1</Nav.Link>
+                <Nav.Link onClick={() => setSelectedPage(defaultPageName)} eventKey="browse">
+                  Browse
+                </Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link onClick={() => setSelectedPage(defaultPageName)} eventKey="second">Tab 2</Nav.Link>
+                <Nav.Link onClick={() => setSelectedPage(defaultPageName)} eventKey="second">
+                  Tab 2
+                </Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link onClick={() => setSelectedPage(defaultPageName)} eventKey="create">Create Buddy Bot</Nav.Link>
+                <Nav.Link onClick={() => setSelectedPage(defaultPageName)} eventKey="create">
+                  <span className="bi bi-plus-square"> Create Buddy Bot</span>
+                </Nav.Link>
               </Nav.Item>
               <Dropdown drop="down-centered" as={NavItem}>
                 <Dropdown.Toggle as={NavLink}>{selectedPage}</Dropdown.Toggle>
                 <Dropdown.Menu>
                   <Dropdown.Item>
                     <Nav.Item>
-                      <Nav.Link onClick={() => setSelectedPage("Profile")} eventKey="profile"><span className="bi bi-person-fill"> Profile</span></Nav.Link>
+                      <Nav.Link onClick={() => setSelectedPage("Profile")} eventKey="profile">
+                        <span className="bi bi-person-fill"> Profile</span>
+                      </Nav.Link>
                     </Nav.Item>
                   </Dropdown.Item>
                   <Dropdown.Item>
                     <Nav.Item>
-                      <Nav.Link onClick={() => setSelectedPage("Favourites")} eventKey="favourite"><span className="bi bi-heart-fill"> Favourites</span></Nav.Link>
+                      <Nav.Link onClick={() => setSelectedPage("Favourites")} eventKey="favourite">
+                        <span className="bi bi-heart-fill"> Favourites</span>
+                      </Nav.Link>
                     </Nav.Item>
                   </Dropdown.Item>
                   <Dropdown.Divider />
@@ -54,11 +64,17 @@ function App() {
           </Col>
           <Col sm={12}>
             <Tab.Content>
-              <Tab.Pane eventKey="logo"><Home /></Tab.Pane>
-              <Tab.Pane eventKey="first">First tab content</Tab.Pane>
+              <Tab.Pane eventKey="logo">
+                <Home />
+              </Tab.Pane>
+              <Tab.Pane eventKey="browse">Browse Buddy Bots</Tab.Pane>
               <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
-              <Tab.Pane eventKey="profile"><Home /></Tab.Pane>
-              <Tab.Pane eventKey="create"><CreateBuddyBot /></Tab.Pane>
+              <Tab.Pane eventKey="profile">
+                <Home />
+              </Tab.Pane>
+              <Tab.Pane eventKey="create">
+                <CreateBuddyBot />
+              </Tab.Pane>
             </Tab.Content>
           </Col>
         </Row>
--- src/Tabs/CreateBuddyBot/CreateBuddyBot.tsx
+++ src/Tabs/CreateBuddyBot/CreateBuddyBot.tsx
@@ -1,50 +1,101 @@
-import { Container, Row, Col, Button, Form, FloatingLabel } from "react-bootstrap"
-import "./CreateBuddyBot.scss"
-import { useState } from "react"
+import { Container, Row, Col, Button, Form, FloatingLabel } from "react-bootstrap";
+import "./CreateBuddyBot.scss";
+import { useState } from "react";
 const CreateBuddyBot = () => {
-    const [form, setForm] = useState<CreateForm>({
-        name: '',
-        language: 'Not Selected'
-    })
-    const handleName = (e: any) => {
-        console.log(form)
-        setForm({
-            ...form,
-            name: e.target.value,
-            language: form.language
-        })
-    }
-    return (
-        <Container fluid>
-            <Form>
-                <Row className="g-2">
-                    <Col md>
-                        <FloatingLabel className="form-text-label" controlId="floatingInputGrid" label="Name">
-                            <Form.Control onChange={handleName} className="form-text" type="text" placeholder="name@example.com" />
-                        </FloatingLabel>
-                    </Col>
-                    <Col md>
-                        <FloatingLabel
-                            className="form-text-label"
-                            controlId="floatingSelectGrid"
-                            label="Language"
-                        >
-                            <Form.Select defaultValue={1} aria-label="Language" className="form-text">
-                                <option value="1">Italian</option>
-                                <option value="2">English</option>
-                                <option value="3">Korean</option>
-                            </Form.Select>
-                        </FloatingLabel>
-                    </Col>
-                </Row>
-                <Row><Button>Ciao</Button></Row>
-            </Form>
-        </Container>
-    )
-}
-export default CreateBuddyBot
+  const [form, setForm] = useState<CreateForm>({
+    name: "",
+    language: "Not Selected",
+  });
+  const levels: string[] = ["A1", "A2", "B1", "B2", "C1", "C2"];
+  const handleName = (e: any) => {
+    console.log(form);
+    setForm({
+      ...form,
+      name: e.target.value,
+      language: form.language,
+    });
+  };
+  return (
+    <Container>
+      <Form>
+        <Row className="g-2">
+          <Col md>
+            <FloatingLabel className="form-text-label" controlId="floatingInputGrid" label="Name">
+              <Form.Control onChange={handleName} className="form-text" type="text" placeholder="name@example.com" />
+            </FloatingLabel>
+          </Col>
+          <Col md>
+            <FloatingLabel className="form-text-label" controlId="floatingSelectGrid" label="Language">
+              <Form.Select defaultValue={1} aria-label="Language" className="form-text">
+                <option value="1">Italian</option>
+                <option value="2">English</option>
+                <option value="3">Korean</option>
+              </Form.Select>
+            </FloatingLabel>
+          </Col>
+          <Col md>
+            <FloatingLabel className="form-text-label" controlId="floatingSelectGrid" label="Level">
+              <Form.Select defaultValue={1} aria-label="Language" className="form-text">
+                {levels.map((level, key) => (
+                  <option value={key} key={key}>
+                    {level}
+                  </option>
+                ))}
+              </Form.Select>
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <Row className="g-2">
+          <Col md>
+            <FloatingLabel controlId="floatingInputGrid" label="Identity">
+              <Form.Control
+                onChange={handleName}
+                className="form-text"
+                style={{ height: "100px" }}
+                as="textarea"
+                placeholder="name@example.com"
+              />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <Row className="g-2">
+          <Col md>
+            <FloatingLabel controlId="floatingInputGrid" label="Setting">
+              <Form.Control
+                onChange={handleName}
+                className="form-text"
+                style={{ height: "100px" }}
+                as="textarea"
+                placeholder="name@example.com"
+              />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <Row className="g-2">
+          <Col md>
+            <FloatingLabel controlId="floatingInputGrid" label="Initial Message">
+              <Form.Control
+                onChange={handleName}
+                className="form-text"
+                style={{ height: "100px" }}
+                as="textarea"
+                placeholder="name@example.com"
+              />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <Row className="centered">
+          <Col sm={1}>
+            <Button variant="secondary">Create</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
+  );
+};
+export default CreateBuddyBot;
 
 interface CreateForm {
-    name: string,
-    language: string,
+  name: string;
+  language: string;
 }
